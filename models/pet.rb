7265db953ffa884a5c3f08a158e34371ea65ce04
dprovder <danielprovder@gmail.com>
2015-07-06 15:15:10 +0000

    pet.rb

--- models/pet.rb
+++ models/pet.rb
@@ -1,16 +1,19 @@
 class Pet
 	attr_accessor :age
 	attr_reader :owner, :name, :gender, :type, :mood
-
-	def initialize(name, age, gender, type)
+    
+	def initialize(name, age, gender, type, mood="")
 		@name = name
-		@mood
+		@mood = mood
 		@type = type
+		@age = age
+		@gender = gender
 	end
 
 	def owner=(owner)
 		if owner.is_a? User
 			@owner = owner
+			@owner.new_pet=(@name, @age, @gender, @type)
 		else
 			"ERROR"
 		end
@@ -20,11 +23,23 @@ class Pet
 		@is_fed = false
 	end
 
-	def feed(food)
-		if food == 
-
+	def feed
 		@is_fed = true
 	end
 
-
+    def walk
+        @mood = "happy"
+    end
+    
+    def speak
+        if :type == "snake"
+            puts "sSsSsS"
+        elsif :type == "dog"
+            puts "woof"
+        else 
+            puts "meow"
+        end
+    end
+    
+    def 
 end
